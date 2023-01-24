8f9fc74c165f718312978200a664de0282de7ac4
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2023-01-25 00:12:10 +0100

    add nav

---
+++ components/Nav/index.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__wrapper">
+        <div className="nav__menu">Menu</div>
+        <div className="nav__menu">Mahdo</div>
+        <div className="nav__menu">Search</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
