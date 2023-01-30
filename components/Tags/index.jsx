0e0ba52561a611bec154e2aa2390badb296c2ce1
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2023-01-30 23:50:17 +0100

    tags component

---
+++ components/Tags/index.jsx
@@ -0,0 +1,11 @@
+const Tags = () => {
+  return (
+    <div className="tags">
+      <span className="tags__btn">Lifestyle,</span>
+      <span className="tags__btn">Thoughts,</span>
+      <span className="tags__btn">Others</span>
+    </div>
+  );
+};
+
+export default Tags;
