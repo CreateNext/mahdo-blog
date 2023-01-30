92db12ff2430d8054afed79d03a871bc7980d26c
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2023-01-30 23:50:05 +0100

    Featured post component

--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -16,4 +16,26 @@ const Post = () => {
   );
 };
 
+export const FeaturedPost = () => {
+  return (
+    <div className="featured-post">
+      <img
+        src="img/preview.jpg"
+        alt="banner"
+        className="featured-post__banner"
+      />
+      <div className="featured-post__info">
+        <span className="featured-post__label">Featured Post</span>
+        <h3 className="featured-post__title">
+          <span>Year</span> <span>In</span> <span>Review?</span>
+        </h3>
+        <p className="featured-post__desc">
+          Don&apos;t Know if I can call this a review, but it&apos;s something!
+        </p>
+        <Link href={"#"}>Read More</Link>
+      </div>
+    </div>
+  );
+};
+
 export default Post;
