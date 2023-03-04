2dfcea9abb2dfbf7a3166b8067eae7d9369c2747
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2023-03-05 00:20:44 +0100

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
+export default function about() {
+  return (
+    <main className="about">
+      <section>
+        <h1 className="about__title">
+          About <span>Mahdo Blog</span>
+        </h1>
+        <div className="about__info">
+          <img
+            src="img/mahdo-alt.png"
+            alt="mahdo blog logo"
+            className="about__img"
+          />
+
+          <p className="about__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+            doloremque a libero adipisci autem deserunt quas ea nemo dolores
+            iusto ex voluptates, dignissimos rerum corporis magnam. Asperiores
+            doloribus accusantium, laudantium alias sed quisquam animi
+            recusandae? Molestias nostrum voluptas excepturi quaerat, suscipit
+            modi consectetur alias, dolore nihil quas nobis corrupti sunt velit
+            temporibus maiores numquam cupiditate ea voluptate qui cumque. Totam
+            reprehenderit facilis tenetur architecto distinctio nisi
+            accusantium, eius eum veritatis, dolorum repellat illo itaque vero
+            qui quod voluptate esse quos perferendis asperiores quia quidem
+            enim, alias minus ipsa. Nulla quam laborum earum eos cum autem ut.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
